2c7968be5a3163e6b84d19e1f9af99545192444f
zhcoder <zhcoder@yandex.ru>
2021-10-26 04:10:22 +1000

    edit test

--- test/hermione/example.hermione.js
+++ test/hermione/example.hermione.js
@@ -6,31 +6,53 @@ function sleep(ms) {
 
 describe('Корзина', async function() {
     it('Проверка оформления заказа + верстка', async function() {
-        //Проверка добавления и очищения элементов
+        //Проверка добавления и очищения элементов + проверка общей стоимости
+        //добавляем первый элемент
         await this.browser.url('https://shri.yandex/hw/store/catalog');
+        let linkDetails = await this.browser.$('.card-link');
+        await sleep(200);
+        await linkDetails.click();
 
-        let link = await this.browser.$(".card-link");
-        await link.waitForExist({ timeout: 1500 })
-        link.click();
-        await sleep(400);
+        let btnAddCard = await this.browser.$('.ProductDetails-AddToCart');
+        await sleep(200);
+        await btnAddCard.click();
 
-        let button = await this.browser.$("//button[text()='Add to Cart']");
-        button.click();
-        await sleep(400);
+        let elementPrice = await this.browser.$('.ProductDetails-Price');
+        await sleep(200);
+        let price = await elementPrice.getText();
+        price = price.toString().replace("$", "");
 
-        let spanSuccessAddItem = await this.browser.$(".text-success");
-        let spanSuccessAddItemText = await spanSuccessAddItem.getText();
-        assert.equal(spanSuccessAddItemText, 'Item in cart');
+        await sleep(200);
+        await this.browser.assertView('item-check', '#root');
 
-        await sleep(600);
-        await this.browser.assertView('itemShop-check', '#root');
+        //добавляем второй элемент в корзину
+        await this.browser.url('https://shri.yandex/hw/store/catalog');
+        linkDetails = await this.browser.$('.row:nth-child(2) div~div .card-link');
+        await sleep(200);
+        await linkDetails.click();
+        btnAddCard = await this.browser.$('.ProductDetails-AddToCart');
+        await sleep(200);
+        await btnAddCard.click();
 
+        let elementPrice2 = await this.browser.$('.ProductDetails-Price');
+        let price2 = await elementPrice2.getText();
+        price2 = price2.toString().replace("$", "");
 
+        //Проверяем в корзине совпадает ли сумма предметов
         await this.browser.url('https://shri.yandex/hw/store/cart');
-        await sleep(550);
+        await sleep(200);
+        let sumPriceElement = await this.browser.$('.Cart-OrderPrice');
+        let sumPrice = await sumPriceElement.getText();
+        sumPrice = sumPrice.toString().replace("$", "");
+
+        price = Number.parseInt(price, 10);
+        price2 = Number.parseInt(price2, 10);
+        sumPrice = Number.parseInt(sumPrice, 10);
+        assert.equal((price + price2), sumPrice);
+        await sleep(250);
         let btnClearCart = await this.browser.$("//button[text()='Clear shopping cart']");
         btnClearCart.click();
-        await sleep(400);
+        await sleep(250);
 
         let linkCatalog = await this.browser.$("//a[text()='catalog']");
         assert.equal(await linkCatalog.getAttribute('href'), "/hw/store/catalog")
@@ -38,49 +60,49 @@ describe('Корзина', async function() {
         //Проверка оформления заказа
         await this.browser.url('https://shri.yandex/hw/store/catalog');
 
-        link = await this.browser.$(".card-link");
-        await link.waitForExist({ timeout: 1500 })
+        let link = await this.browser.$(".card-link");
+        await link.waitForExist({ timeout: 600 })
         link.click();
-        await sleep(400);
+        await sleep(200);
 
-        button = await this.browser.$("//button[text()='Add to Cart']");
+        let button = await this.browser.$("//button[text()='Add to Cart']");
         button.click();
-        await sleep(400);
+        await sleep(200);
 
-        spanSuccessAddItem = await this.browser.$(".text-success");
-        spanSuccessAddItemText = await spanSuccessAddItem.getText();
+        let spanSuccessAddItem = await this.browser.$(".text-success");
+        let spanSuccessAddItemText = await spanSuccessAddItem.getText();
         assert.equal(spanSuccessAddItemText, 'Item in cart');
 
         await this.browser.url('https://shri.yandex/hw/store/cart')
         this.browser.refresh();
 
         let inputName = await this.browser.$('.Form-Field_type_name')
-        await sleep(250);
+        await sleep(200);
         await inputName.scrollIntoView();
         await inputName.click();
         await inputName.keys(["Nikita"]);
 
         let inputPhone = await this.browser.$('.Form-Field_type_phone');
-        await sleep(250);
+        await sleep(200);
         await inputPhone.scrollIntoView();
         await inputPhone.click();
         await inputPhone.keys(["81234567890"]);
 
         let formAddress = await this.browser.$('.Form-Field_type_address');
-        await sleep(250);
+        await sleep(200);
         await formAddress.scrollIntoView();
-        await this.browser.waitUntil(() => formAddress.isClickable(), 700);
+        await this.browser.waitUntil(() => formAddress.isClickable(), 600);
         await formAddress.click();
         await formAddress.keys(["Test address"]);
 
-        await sleep(250);
+        await sleep(200);
         const btnCheckout = await this.browser.$('.Form-Submit');
         await btnCheckout.scrollIntoView();
 
-        await sleep(250);
+        await sleep(200);
         await btnCheckout.click();
 
-        await sleep(450);
+        await sleep(250);
         const alertHeading = await this.browser.$('.alert-heading');
         assert.equal(await alertHeading.getText(), 'Well done!');
     });
